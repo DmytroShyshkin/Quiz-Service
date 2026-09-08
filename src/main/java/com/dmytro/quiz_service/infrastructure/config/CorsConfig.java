2111a34995280df0ffb78e0be3268d29229914d0
Dmytro Shyshkin <dmytro.dev.shyshkin@gmail.com>
2026-09-08 13:14:05 +0200

    Update CorsConfig.java

--- src/main/java/com/dmytro/quiz_service/infrastructure/config/CorsConfig.java
+++ src/main/java/com/dmytro/quiz_service/infrastructure/config/CorsConfig.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class CorsConfig {
     private final CorsProperties corsProperties;
 
